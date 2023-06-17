c88a1e5770075a1fd3f0c360fb293965099c856d
Aadithya Kandeth <aadithya9788@gmail.com>
2023-06-17 03:55:13 -0400

    End of Section 4 (Authentication)

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,5 +6,9 @@ namespace API.Entities
     {
         public int Id { get; set; }      
         public string UserName { get; set; }  //string? indicates nullable
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
