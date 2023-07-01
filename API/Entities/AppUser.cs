9285a1b8bc24e1ea2ddc85b00f1187fc28f7a0eb
Aadithya Kandeth <aadithya9788@gmail.com>
2023-07-01 02:07:35 -0400

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
 using System.Security.AccessControl;
+using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
@@ -6,9 +8,24 @@ namespace API.Entities
     {
         public int Id { get; set; }      
         public string UserName { get; set; }  //string? indicates nullable
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new();
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
+
     }
 }
